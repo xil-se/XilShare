feeb7f1ddaff640e0f629f3a9b17cd623b2830e0
Jens Andersen <jens.andersen@gmail.com>
2019-06-01 12:32:54 +0200

    Upgrade to latest dependencies, add clickable PP link

--- app/src/main/java/se/xil/xilshare/MainActivity.java
+++ app/src/main/java/se/xil/xilshare/MainActivity.java
@@ -1,12 +1,15 @@
 package se.xil.xilshare;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
